bd5d7002a34b8f2780d8efd29341e148ddb1f5e5
Koki <kgoudy64@gmail.com>
2022-02-01 20:49:11 -0600

    Create app.js
    Creating generic object for backpack

---
+++ app.js
@@ -0,0 +1,18 @@
+const backpack = {
+  name: "Everyday Backpack",
+  volume: 30,
+  color: "grey",
+  pocketNum: 15,
+  strapLength: {
+    left: 26,
+    right: 26,
+  },
+  lidOpen: false,
+  toggleLid: function (lidStatus) {
+    this.lidOpen = lidStatus;
+  },
+  newStrapLength: function (lenthLeft, lengthRight) {
+    this.strapLength.left = lengthLeft;
+    this.strapLength.right = lengthRight;
+  },
+};
